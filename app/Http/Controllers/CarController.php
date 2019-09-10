1fa424111ae7d00835d81149f4a66cb82615c144
nishankhadka <nishankhadka10.nk@gmail.com>
2019-09-10 12:42:55 +0545

    Insert car detail by Admin

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Car;
+use \App\Car;
 // Insert Car 
 class CarController extends Controller
 {
@@ -11,19 +11,51 @@ class CarController extends Controller
     public function insert(){
         return view('admin/addCar');
     }
-    public function store()
+    public function store(Request $r)
     {
-    	Car::create([
-    		'car_name'=>request('car_name'),
-    		'car_model'=>request('car_model'),
-    		'car_description'=>request('car_description'),
-    		'price'=>request('price'),
-    		'capacity'=>request('capacity'),
-    		'fuel_type'=>request('fuel_type'),
-    		'aircondition'=>request('aircondition'),
-    		'image'=>request('image'),
+    	// Car::create([
+    	// 	'car_name'=>request('car_name'),
+    	// 	'car_model'=>request('car_model'),
+    	// 	'car_description'=>request('car_description'),
+    	// 	'price'=>request('price'),
+    	// 	'capacity'=>request('capacity'),
+    	// 	'fuel_type'=>request('fuel_type'),
+    	// 	'aircondition'=>request('aircondition'),
+    	// 	'image'=>request('image'),
 
-    	]);
+    	// ]);
+         $validation = array(
+            'name'=>'required',
+            'model'=>'required',
+            'price'=>'required|integer',
+            'capacity'=>'required|integer',
+            'description'=>'required',
+            'image'=>'mimes:jpeg,bmp,png,gif|max:3500'
+            );
+
+    $r->validate($validation);
+
+        $imgname = '';
+        if ($r->hasfile('image')) {
+            $file = $r->file('image');
+            $imgname = date('ymdhis').$file->getClientOriginalName();
+            $path = public_path().'/uploads/';
+            $file->move($path,$imgname);
+        }
+
+        $car = new Car;
+        $car->car_name = $r->get('name');
+        $car->car_model = $r->get('model');
+        $car->price = $r->get('price');
+        $car->car_description = $r->get('description');
+        $car->capacity = $r->get('capacity');
+        $car->image = $imgname;
+        $car->fuel_type = $r->get('fuel_type');
+        $car->aircondition = $r->get('ac');
+        $car->save();
+
+        return redirect('/admin/createcar')->with('msg','Car added successfully');
+
     }
 
 
