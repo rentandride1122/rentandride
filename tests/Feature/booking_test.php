1e4f4f77c01ac2e5156d1ab361b07f8870fbe5a7
unknown <nabinkhanal32@gmail.com>
2019-10-16 07:25:10 +0545

    booking update test

--- tests/Feature/booking_test.php
+++ tests/Feature/booking_test.php
@@ -34,4 +34,24 @@ class booking_test extends TestCase
         $this->assertCount(1,Booking::all());
 
     }
+
+    public function booking_car_update()
+    {
+        $this->withoutExceptionHandling();
+    
+        $book = Booking::first();
+        
+        $response= $this->patch('/client/updatebooking/'.$car->id,[
+            'Destination'=>'butwal',
+            'current_location'=>'bhaktpur',
+            'days'=>'4',
+            'user_id' => $user->id,
+        ]);
+        $this->assertEquals('butwal',Booking::first()->Destination);
+        $this->assertEquals('bhaktpur',Booking::first()->current_location);
+        $this->assertEquals('4',Booking::first()->days);
+        $this->assertEquals($user->id,Booking::first()->user_id);
+
+
+    }
 }
