45bae5374a8de31a00bad16f7a6642c421c67ce1
RudraRajbanshi <rudra.raj.716@gmail.com>
2019-10-17 07:50:52 +0545

    Create booking table

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,9 @@ Route::get('/user/logout','UserController@logout')->name('user.logout');
 
 Route::get('/user/viewcars','UserCarController@viewcar')->name('user.viewcar');
 Route::get('/user/yourcar','UserCarController@viewyourcar')->name('user.viewyourcar');
+Route::get('/user/yourcar/view/{id}','UserCarController@yourcardesc')->name('user.yourcardesc');
+Route::get('/user/update/yourcar/{id}','UserCarController@updateyourcar')->name('user.updateyourcar');
+Route::delete('/user/delete/yourcar','UserCarController@deleteyourcar')->name('user.deleteyourcar');
 
 //testing privatecar
 
